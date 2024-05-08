3d6e706c663e2b6e777f87ad7b6d977d89e48af4
Simon Bauer <simon.bauer@josephinum.at>
2024-05-08 16:27:37 +0200

    Add project files.

---
+++ MathUtils_WithAOT/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace MathUtils_WithAOT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public static class MathUtils
+    {
+        public const double PI = Math.PI;
+        public const double e = Math.E;
+        public const double g = 9.81;
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static double CircleArea(int x)
+        {
+            return x * x * PI;
+        }
+        public static double Circumference(int x)
+        {
+            return (x + x) * PI;
+        }
+        public static double FallSpeedDistance(int n)
+        {
+            return n * g;
+        }
+        public static double FallDistance(int x)
+        {
+            return 0.5 * g * x * x;
+        }
+        public static double SquareArea(int x)
+        {
+            return x * x;
+        }
+        public static double Diameter(int u)
+        {
+            return u / PI;
+        }
+    }
+}
